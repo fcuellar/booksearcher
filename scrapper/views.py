514c24dcfad9de24ec849b4077e7078e510136ce
fcuellar <frankiecuellar@hotmail.com>
2021-06-13 00:28:38 -0400

    added requirements, search and main page functionality

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 def home(requests):
     return render(requests, 'scrapper/base.html')
+
+def new_search(requests):
+    search=requests.POST.get('search')
+    print(search)
+    stuff_for_frontend={
+        'search':search,
+    }
+    return render(requests,'scrapper/new_search.html',stuff_for_frontend)
